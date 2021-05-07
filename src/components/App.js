05fc74825d3574546d9ad6c00447a284571c8df9
Annie Moosher <annie.sophia219@gmail.com>
2021-05-07 14:30:45 -0400

    added menu and tab styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,30 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./ui/theme.js";
 import Header from "./ui/Header.js";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello!
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route
+            exact
+            path="/createmeal"
+            component={() => <div>Create meal</div>}
+          />
+          <Route exact path="/login" component={() => <div>Login</div>} />
+          <Route exact path="/signup" component={() => <div>Signup</div>} />
+          <Route
+            exact
+            path="/browse"
+            component={() => <div>Browse Meals</div>}
+          />
+          <Route exact path="/host" component={() => <div>Host</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Link } from "react-router-dom";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/styles";
 
@@ -36,7 +40,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "3em",
   },
   logo: {
-    height: "7em",
+    height: "8em",
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
   },
   tabContainer: {
     marginLeft: "auto",
@@ -49,21 +59,152 @@ const useStyles = makeStyles((theme) => ({
     //minWidth: 10 This squeezes tabs together with a bit of space between
     //marginLeft: "25px" Want to maintain constant spacing
   },
+  /*
+  button: {
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    //You can also specify fontFamily and fontSize, textTransform and height
+  },
+  */
+  menu: {
+    backgroundColor: theme.palette.common.pink,
+    color: "#fff100",
+  },
+  menuItem: {
+    ...theme.typography.tab,
+  },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/createmeal" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/login" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/signup" && value !== 3) {
+      setValue(3);
+    }
+  }, [value]);
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters>
-            <img src={logo} alt="app logo" className={classes.logo} />
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Create Event" />
-              <Tab className={classes.tab} label="Login" />
-              <Tab className={classes.tab} label="SignUp" />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img src={logo} alt="app logo" className={classes.logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="secondary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                aria-haspopup={anchorEl ? "true" : undefined}
+                onMouseOver={(event) => handleClick(event)}
+                className={classes.tab}
+                component={Link}
+                to="/createmeal"
+                label="Create Meal"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/login"
+                label="Login"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/signup"
+                label="SignUp"
+              />
             </Tabs>
+            {/* <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Holder
+           </Button>*/}
+            <Menu
+              id="simple-menu"
+              classes={{ paper: classes.menu }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+              elevation={0}
+            >
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/createmeal"
+              >
+                Create Meal
+              </MenuItem>
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/browse"
+              >
+                Browse Meal Outings
+              </MenuItem>
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/host"
+              >
+                Host Meal Outing
+              </MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
